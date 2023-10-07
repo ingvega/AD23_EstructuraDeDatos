ab4aea42419457f1b7514ba3b9febbcb6304f6c3
ingvega <isc.p.vega@gmail.com>
2023-10-07 12:19:50 -0600

    Repaso para examen

--- unidad3/ED23_U3/src/RepasoRecursividad.java
+++ unidad3/ED23_U3/src/RepasoRecursividad.java
@@ -9,11 +9,37 @@
  * @author paveg
  */
 public class RepasoRecursividad {
-
+    Object[][] matriz=new Object[][]{
+            {4,"Juan"},
+            {7,"Luis"},
+            {1,"Ana"},
+            {90,"Raúl"},
+            {2,"Rosa"},
+            {10,"Iván"}
+        };
     
-    public static void main(String[] args) {
+    public static void main(String[] args) {   
         RepasoRecursividad r = new RepasoRecursividad();
-        System.out.println(r.generarPalindromo("Hola"));
+//        System.out.println(r.generarPalindromo("Hola"));
+        
+        System.out.println(r.buscar(10));
+    }
+    
+    public String buscar(int valor){
+        return buscar(valor,0);
+    }
+    
+    private String buscar(int valor,int pos){
+        if(matriz.length==pos){
+            //Caso base
+            return "Alumno no encontrado";
+        }else{
+            if(matriz[pos][0].equals(valor)){
+                return matriz[pos][1].toString();
+            }else{
+                return buscar(valor,pos+1);
+            }
+        }
     }
 
     public String generarPalindromo(String cadena) {
